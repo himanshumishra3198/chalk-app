be0635a9caf721be513115db56b5fcfba4592300
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-03-17 20:10:47 +0530

    changed url

--- apps/chalk-frontend/app/configs/index.ts
+++ apps/chalk-frontend/app/configs/index.ts
@@ -1,2 +1,5 @@
-export const BACKEND_URL = "http://localhost:8080";
-export const WEBSOCKET_URL = "ws://localhost:8081";
+// export const BACKEND_URL = "http://localhost:8080";
+// export const WEBSOCKET_URL = "ws://localhost:8081";
+
+export const BACKEND_URL = "http://3.107.102.79/api";
+export const WEBSOCKET_URL = "ws://3.107.102.79/ws";
