c346dc2aed8d14edbf425355775d5a12e141eaaf
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-03-19 01:00:29 +0530

    updated url

--- apps/chalk-frontend/app/configs/index.ts
+++ apps/chalk-frontend/app/configs/index.ts
@@ -1,5 +1,2 @@
-// export const BACKEND_URL = "http://localhost:8080";
-// export const WEBSOCKET_URL = "ws://localhost:8081";
-
-export const BACKEND_URL = "http://139.59.87.42/api";
-export const WEBSOCKET_URL = "ws://139.59.87.42/ws";
+export const BACKEND_URL = "http://139.59.87.42/:8080/api";
+export const WEBSOCKET_URL = "ws://139.59.87.42/:8081/ws";
