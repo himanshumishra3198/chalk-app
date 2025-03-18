53fe74ceb9d6beef900c5866a161ff5a2cccc779
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-03-18 19:52:46 +0530

    debugging ci/cd

--- apps/chalk-frontend/app/configs/index.ts
+++ apps/chalk-frontend/app/configs/index.ts
@@ -1,5 +1,5 @@
 // export const BACKEND_URL = "http://localhost:8080";
 // export const WEBSOCKET_URL = "ws://localhost:8081";
 
-export const BACKEND_URL = "http://3.107.102.79/";
-export const WEBSOCKET_URL = "ws://3.107.102.79/ws";
+export const BACKEND_URL = "http://139.59.87.42/api";
+export const WEBSOCKET_URL = "ws://139.59.87.42/ws";
