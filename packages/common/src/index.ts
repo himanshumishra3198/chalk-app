86d9d189948a16ce87f2e57c911fc2a053bc50a1
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-03-23 22:21:10 +0530

    adding more space to email

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
-  email: z.string().min(3).max(20),
+  email: z.string().max(60),
   password: z.string(),
   name: z.string(),
 });
 
 export const SigninUserSchema = z.object({
-  email: z.string().min(3).max(20),
+  email: z.string(),
   password: z.string(),
 });
 
 export const CreateRoomSchema = z.object({
-  slug: z.string().min(3).max(20),
+  slug: z.string(),
 });
 
 export const JWT_SECRET = process.env.JWT_SECRET || "1234";
