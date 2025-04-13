0ae45abbda9c34fc3e91376e332fc7df8918cca1
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-04-13 06:34:43 +0530

    fixed certificates and configs url

--- apps/chalk-frontend/app/configs/index.ts
+++ apps/chalk-frontend/app/configs/index.ts
@@ -1,12 +1,9 @@
-// export const BACKEND_URL = "http://128.199.27.128/api";
-// export const WEBSOCKET_URL = "ws://128.199.27.128/ws/";
-
 const isLocal = process.env.NODE_ENV === "development";
 
 export const BACKEND_URL = isLocal
   ? "http://localhost:8080" // Use localhost for development
-  : "http://128.199.27.128/api"; // Use production URL for deployment
+  : "https://chalk.hm0.org/api"; // Use production URL for deployment
 
 export const WEBSOCKET_URL = isLocal
   ? "ws://localhost:8081" // Use localhost for development
-  : "ws://128.199.27.128/ws/"; // Use production URL for deployment
+  : "ws://chalk.hm0.org/ws/"; // Use WSS (secure WebSocket) in production
